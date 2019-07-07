af104c9c41eb645845088c3f3b03bbc5d0162e68
Haojie Chen <hxc889@cs.bham.ac.uk>
2019-07-07 20:44:55 +0100

    Camera set up

---
+++ Programs/CameraSetUp.py
@@ -0,0 +1,26 @@
+import cv2 as cv
+
+
+cap = cv.VideoCapture(0)
+
+print(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+print(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+
+cap.set(cv.CAP_PROP_FRAME_WIDTH, 5000)
+cap.set(cv.CAP_PROP_FRAME_HEIGHT, 5000)
+
+print(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+print(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+
+while cap.isOpened():
+  ret, frame = cap.read()
+  if ret == True:
+    cv.imshow('frame', frame)
+
+    if cv.waitKey(50) & 0xFF == ord('q'):
+      break
+  else:
+    break
+
+cap.release()
+cv.destroyAllWindows()
