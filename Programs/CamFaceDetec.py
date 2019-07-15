ae9226a7a3c707525a65c4daaa7ef9bb90089530
Haojie Chen <hxc889@cs.bham.ac.uk>
2019-07-15 14:11:06 +0100

    face detection through cam

---
+++ Programs/CamFaceDetec.py
@@ -0,0 +1,27 @@
+import cv2
+
+
+cap = cv2.VideoCapture(0)
+faceCascate = cv2.CascadeClassifier('/usr/local/lib/python3.7/site-packages/cv2/data/haarcascade_frontalface_default.xml')
+
+
+def scaleFace(frame):
+    faces = faceCascate.detectMultiScale(frame, 1.05, 10)
+    for (x, y, w, h) in faces:
+        # Draw rectangle around the face
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 255), 1)
+
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if ret:
+        scaleFace(frame)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+    else:
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
